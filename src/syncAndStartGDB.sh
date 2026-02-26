abdea83b37cbc84b2c5190fa11e0abbe75b41e88
Jérémie Blais <jereblais@gmail.com>
2026-02-26 00:48:06 +0000

    rasppi add

--- src/syncAndStartGDB.sh
+++ src/syncAndStartGDB.sh
@@ -3,7 +3,7 @@ set -e
 
 # Sync executable
 bn=$(basename $1)
-rpiaddr="inserer_adresse_de_votre_raspberry_pi_ici"
+rpiaddr="jebla104.dedyn.io"
 
 if [ "$2" == "all" ]; then
     rsync -az $1/build/compositeur $1/build/decodeur $1/build/convertisseur $1/build/filtreur $1/build/redimensionneur "pi@$rpiaddr:/home/pi/projects/laboratoire3/"
